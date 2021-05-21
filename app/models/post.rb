2cd1b1e235708eb4098aee4c71d668fa88350bf4
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-05-21 17:36:15 +0200

    add post association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-     belongs_to :user
+      belongs_to :user
+      has_many :comments
 
      validates :content, presence: { message: "must be given" },
                length: { maximum: 40000, message: "must be less than 50 charcaters"}
