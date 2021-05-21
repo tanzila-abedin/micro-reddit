c13009dec912466233da4028643f6f1064fd1b0b
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-05-21 15:13:51 +0200

    confirmed working valication on console

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    validates :name,  presence: true { message: "must be given please" }, 
+    validates :name,  presence: { true, message: "must be given please" },
                       length: { maximum: 50, message: "must be less thna 50 charcaters" }
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true { message: "must be given please" }, 
+    validates :email, presence: { true, message: "must be given please" }, 
                     length: { maximum: 255, message: "must be less than 255 characters" },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
