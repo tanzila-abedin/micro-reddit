e365913b9bc505d810bf6a749d45f49f8032ea52
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-05-21 16:27:20 +0200

    add user posts validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+     belongs_to :user
+
+     validates :content, presence: { message: "must be given" },
+               length: { maximum: 40000, message: "must be less than 50 charcaters"}
 end
