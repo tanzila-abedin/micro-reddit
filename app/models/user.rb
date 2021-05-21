9670aa96b902e7719450e8e8ab69ddb2addbb518
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-05-21 16:26:49 +0200

    update mx num char for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-    validates :name,  presence: { true, message: "must be given please" },
+    has_many :posts
+
+    validates :name,  presence: { message: "must be given please" },
                       length: { maximum: 50, message: "must be less thna 50 charcaters" }
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: { true, message: "must be given please" }, 
-                    length: { maximum: 255, message: "must be less than 255 characters" },
+    validates :email, presence: { message: "must be given please" }, 
+                    length: { maximum: 320, message: "must be less than 320 characters" },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
      
