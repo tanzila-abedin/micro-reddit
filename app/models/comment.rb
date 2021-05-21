3119019b18f17c18e0fe66dd6b94148bfb6fb069
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-05-21 17:36:53 +0200

    add comment association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+      belongs_to :post
+      belongs_to :user
+
       validates :content, presence: { message: "must be given" },
                           length: { maximum: 8000 , message: "must be less than 50 charcaters"}
 end
