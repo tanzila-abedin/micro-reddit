92dd7694011f0b545441afe13b3ca23bbd99aa06
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-05-22 18:07:31 +0200

    validate comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
       belongs_to :post
       belongs_to :user
 
-      validates :content, presence: { message: "must be given" },
+      validates :comment, presence: { message: "must be given" },
                           length: { maximum: 8000 , message: "must be less than 50 charcaters"}
 end
